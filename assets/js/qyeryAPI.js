2a6b5039cc48491e37a53aa09b7c9d527c2bb226
HCAC17 <henriquecaffagn.com.br@gmail.com>
2022-05-04 11:43:57 -0300

    Facebook 2 - 04/05/2022

--- assets/js/qyeryAPI.js
+++ assets/js/qyeryAPI.js
@@ -1,5 +1,7 @@
 var imputOrigem = document.getElementById('input1');
 var imputDestino = document.getElementById('input2');
+var email = document.getElementById('input3')
+var senha = document.getElementById('input4')
 var inputChat = document.getElementById('inputChat')
 var campoChat = document.getElementById('aqui')
 
@@ -7,10 +9,65 @@ var interval  = undefined;
 
 var obj = {}
 
-imputDestino.value = 'gabs';
-imputOrigem.value = 'henriques';
-inputChat.value = 'eae vei blz?';
+pesquisaCadas(); 
 
+inputChat.value = 'eae vei blz?';
+senha.value = 123456; 
+email.value = "henrique.carneiro@cliqx.com.br"; 
+
+function login () {
+    var xhr = new XMLHttpRequest();
+    console.log(email.value, senha.value)
+    xhr.open("GET",`https://barth.com.br/ApiChatCliqx/chat/verificarLogin.php?email=${email.value}&senha=${senha.value}`);
+    xhr.send(null);
+
+    xhr.onreadystatechange = function () {
+        console.log(JSON.parse(xhr.responseText))
+      if (xhr.readyState === 4) {
+        //console.log("aquifoi");
+        if (xhr.status === 200) {
+
+            //campoChat.innerHTML='';
+            response = JSON.parse(xhr.responseText);
+            if(response.login){
+                imputDestino.disabled  = false;
+                inputChat.disabled  = false;
+                imputOrigem.value = response.nome
+            }
+            //console.log(response);
+        }
+      }
+    };
+  }
+
+
+  function pesquisaCadas () {
+    var xhr = new XMLHttpRequest();
+    console.log(email.value, senha.value)
+    xhr.open("GET",`https://barth.com.br/ApiChatCliqx/chat/receberUsuarios.php`);
+    xhr.send(null);
+
+    xhr.onreadystatechange = function () {
+        console.log(JSON.parse(xhr.responseText))
+      if (xhr.readyState === 4) {
+        //console.log("aquifoi");
+        if (xhr.status === 200) {
+          
+            //campoChat.innerHTML='';
+            response = JSON.parse(xhr.responseText);
+
+            for(let i = 0; i < response.length; i++){
+              var opition = document.createElement('option');
+              var nomeOp = document.createTextNode(response[i].nome);
+              opition.appendChild(nomeOp);
+              opition.setAttribute('value', `${response[i].nome}`);
+              imputDestino.appendChild(opition);
+            }
+            //console.log(response);
+        }
+      }
+    };
+  }
 
 function pesquisarAPI () {
     var xhr = new XMLHttpRequest();
@@ -31,6 +88,7 @@ function pesquisarAPI () {
             
             for(let x = 0; x < response.length; x++){
                 var li = document.createElement('li');
+
                 var dt = document.createElement('dt');
                 var dtText = document.createTextNode(
                     `${response[x].origem}`
@@ -40,7 +98,7 @@ function pesquisarAPI () {
 
                 var dd = document.createElement('dd');
                 var ddText = document.createTextNode(
-                    `${response[x].mensagem}`
+                    `${response[x].mensagem}`  
                 )
                 dd.appendChild(ddText);
                 li.appendChild(dd);
@@ -73,7 +131,7 @@ function chat () {
             
             //console.log(xhr.status)
             if (xhr.status == 201) {
-                //console.log('mando')
+                console.log('mando');
                 interval = setInterval(function(){
                     pesquisarAPI();
                 },5000)
